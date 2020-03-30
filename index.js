b51909a11c5a56641afe6efa94fe70ccb299ae35
mojtaba mohammadi <49897989+mojtabams@users.noreply.github.com>
2020-03-30 23:33:28 +0430

    input search

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-console.log('Hi there!');
+const fetchData = async (search) => {
+    const response = await axios.get('http://www.omdbapi.com/', {
+        params: {
+            apikey: 'c4568ff9',
+            s: search,
+        },
+    });
+    console.log(response.data);
+};
+
+const input = document.querySelector('input');
+const debounce = (func, timeOut = 1000) => {
+
+    let timeoutID;
+    return (...args) => {
+        if (timeoutID) {
+            clearTimeout(timeoutID);
+        }
+        timeoutID = setTimeout(() => {
+            func.apply(null, args);
+        }, timeOut)
+
+    };
+};
+
+const onInput = (event) => {
+        fetchData(event.target.value)
+    };
+
+input.addEventListener('input', debounce(onInput,500));
