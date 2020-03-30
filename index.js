8fbfd5e196ab47f721681e3b1dc8f9f497d109c2
mojtaba mohammadi <49897989+mojtabams@users.noreply.github.com>
2020-03-31 00:48:43 +0430

    search detail

--- index.js
+++ index.js
@@ -12,18 +12,30 @@ const fetchData = async searchTerm => {
     return response.data.Search;
 };
 
-const input = document.createElement('input');
-document.querySelector('#container').appendChild(input);
+const container = document.querySelector('#container');
+container.innerHTML = `
+    <label><b>Search For a Movie</b></label>
+    <input class="input">
+    <div class="dropdown">
+        <div class="dropdown-menu">
+          <div class="dropdown-content results"></div>
+        </div>
+      </div>
+`
+const input = document.querySelector("input");
+const dropdown = document.querySelector(".dropdown");
+const results = document.querySelector(".results");
 
 const onInput = async event => {
     const movies = await fetchData(event.target.value);
+    dropdown.classList.add('is-active');
     for (let movie of movies) {
         const div = document.createElement('div');
         div.innerHTML = `
             <img src="${movie.Poster}" />
             <h1>${movie.Title}</h1>
             `;
-        document.querySelector('#target').appendChild(div);
+        results.appendChild(div);
     }
 };
 
